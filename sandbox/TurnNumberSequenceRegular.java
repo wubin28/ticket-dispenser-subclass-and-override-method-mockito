fb8b21076d69e4275582250ce0ce91feee4442a2
frog_56CCABAE01 <frog@cyber-dojo.org>
2015-12-09 10:38:21 +0000

    13

--- sandbox/TurnNumberSequenceRegular.java
+++ sandbox/TurnNumberSequenceRegular.java
@@ -1,2 +1,8 @@
 public class TurnNumberSequenceRegular {
+    private static int turnNumber;
+
+    public TurnNumberSequenceRegular(int startTurnNumber) {
+        turnNumber = startTurnNumber;
+    }
+
 }
