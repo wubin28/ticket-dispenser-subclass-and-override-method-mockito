0e3927371f2542ca6d8971c14687e44c1c12e84e
frog_56CCABAE01 <frog@cyber-dojo.org>
2015-12-09 10:39:15 +0000

    14

--- sandbox/TurnNumberSequenceRegular.java
+++ sandbox/TurnNumberSequenceRegular.java
@@ -1,8 +1,13 @@
-public class TurnNumberSequenceRegular {
+public class TurnNumberSequenceRegular extends TurnNumberSequence {
     private static int turnNumber;
 
     public TurnNumberSequenceRegular(int startTurnNumber) {
         turnNumber = startTurnNumber;
+    }
+
+    @Override
+    public int getNextTurnNumber() {
+        return turnNumber++;
     }
 
 }
